bc725410edda43d8b84e7710b5d0ebe1b704eb7d
77web <info@77-web.com>
2022-06-10 22:50:26 +0900

    駅間の距離ポイントを計算するため、駅に「始発駅からの距離ポイント」をもたせる

--- src/Station.php
+++ src/Station.php
@@ -7,11 +7,17 @@ class Station
 {
     public function __construct(
         private readonly string $name,
+        private readonly int $distanceFromStartStation,
     ) {
     }
 
     public function getName(): string
     {
         return $this->name;
+    }
+    
+    public function getDistanceFromStartStation(): int
+    {
+        return $this->distanceFromStartStation;
     }
 }
