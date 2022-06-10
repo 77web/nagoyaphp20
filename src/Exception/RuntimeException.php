e1df38f0903a76314108473141a3ece0062c41a6
77web <info@77-web.com>
2022-06-10 22:37:20 +0900

    composer create-project koriym/php-skeleton

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nagoya\Php20\Exception;
+
+class RuntimeException extends \RuntimeException
+{
+}
---
+++ src/Php20.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nagoya\Php20;
+
+final class Php20
+{
+}
